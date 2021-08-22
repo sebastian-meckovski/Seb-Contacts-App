944fbdf245d9b4fd916e8c98bb88f6874fdf1fd8
Sebastian Meckovski <59857424+sebastian-meckovski@users.noreply.github.com>
2021-08-22 13:04:54 +0100

    contact class created. Listview bg color changed

---
+++ Classes/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace SebContactsApp.Classes
+{
+    class Contact
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public string Company { get; set; }
+        public string position { get; set; }
+        public string mobile { get; set; }
+        public string email { get; set; }
+    }
+}
