7d6912f56e89ee94eb252b306c26aad7953647fb
Sebastian Meckovski <59857424+sebastian-meckovski@users.noreply.github.com>
2021-09-05 10:55:08 +0100

    create folder and initialise db comands created

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -82,6 +82,7 @@ namespace SebContactsApp
             this.Controls.Add(this.listView1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
